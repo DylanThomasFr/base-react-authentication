0ca45bc89bb42c132fc736cffbba7492be9bee0d
Dylan Toomas <thomas.dylan@fdvhuset.no>
2022-05-05 17:33:43 +0200

    Add base project

---
+++ src/components/Auth/AuthForm.jsx
@@ -0,0 +1,81 @@
+import { useState, useRef, useContext } from 'react';
+
+import classes from './AuthForm.module.css';
+import AuthContext from "../../store/AuthContext";
+
+const AuthForm = () => {
+    const [isLogin, setIsLogin] = useState(true);
+    const emailInputRef = useRef()
+    const passwordInputRef = useRef()
+    const context = useContext(AuthContext);
+
+    const switchAuthModeHandler = () => {
+        setIsLogin((prevState) => !prevState);
+    };
+
+    const sendRequest = async (url, email, password) => {
+        const response = await fetch(
+            url,
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    email,
+                    password,
+                    returnSecureToken: true
+                }),
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            }
+        )
+
+        if(response.ok) {
+            return response.json()
+        }
+        throw new Error('Authentication failed !')
+    }
+
+    const submitHandler = async (event) => {
+        event.preventDefault()
+
+        const enteredEmail = emailInputRef.current.value
+        const enteredPassword = passwordInputRef.current.value
+
+        const url = `https://identitytoolkit.googleapis.com/v1/accounts:${isLogin ? 'signInWithPassword' : 'signUp'}?key=${process.env.VITE_APP_FIREBASE_API_KEY}`
+
+        try {
+            const data = await sendRequest(url, enteredEmail, enteredPassword)
+            context.login(data.idToken)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    return (
+        <section className={classes.auth}>
+            <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+            <form onSubmit={submitHandler}>
+                <div className={classes.control}>
+                    <label htmlFor='email'>Your Email</label>
+                    <input ref={emailInputRef} type='email' id='email' required />
+                </div>
+                <div className={classes.control}>
+                    <label htmlFor='password'>Your Password</label>
+                    <input ref={passwordInputRef} type='password' id='password' required />
+                </div>
+                <div className={classes.actions}>
+                    <button>{isLogin ? 'Login' : 'Create Account'}</button>
+                    <button
+                        type='button'
+                        className={classes.toggle}
+                        onClick={switchAuthModeHandler}
+                    >
+                        {isLogin ? 'Create new account' : 'Login with existing account'}
+                    </button>
+                </div>
+            </form>
+        </section>
+    );
+};
+
+export default AuthForm;
---
+++ src/components/Layout/MainNavigation.jsx
@@ -0,0 +1,41 @@
+import {Fragment} from "react";
+import {useContext} from "react";
+import { Link } from 'react-router-dom';
+
+import classes from './MainNavigation.module.css';
+import AuthContext from "../../store/AuthContext";
+
+const MainNavigation = () => {
+    const context = useContext(AuthContext)
+    return (
+        <header className={classes.header}>
+            <Link to='/'>
+                <div className={classes.logo}>React Auth</div>
+            </Link>
+            <nav>
+                <ul>
+                    {
+                        !context.isLoggedIn &&
+                        <li>
+                            <Link to='/auth'>Login</Link>
+                        </li>
+                    }
+                    {
+                        context.isLoggedIn && (
+                            <Fragment>
+                                <li>
+                                    <Link to='/profile'>Profile</Link>
+                                </li>
+                                <li>
+                                    <button>Logout</button>
+                                </li>
+                            </Fragment>
+                        )
+                    }
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default MainNavigation;
